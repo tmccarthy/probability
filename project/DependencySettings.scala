f0092f7f7985653e0a24ecb92065ffba23a0fd0e
Timothy McCarthy <ebh042@gmail.com>
2019-09-25 09:16:48 +1000

    Fix testkit error

--- project/DependencySettings.scala
+++ project/DependencySettings.scala
@@ -19,9 +19,7 @@ object DependencySettings {
   val spireDependency = libraryDependencies += "org.typelevel" %% "spire" % "0.17.0-M1"
 
   val catsTestKitDependency = libraryDependencies ++= List(
-    "org.typelevel" %% "cats-testkit" % "2.0.0" % Test,
-    "org.typelevel" %% "cats-testkit-scalatest" % "1.0.0-M2" % Test,
-    "org.scalatest" %% "scalatest" % "3.1.0-RC2" % Test,
+    "org.typelevel" %% "cats-testkit" % "2.0.0-M4" % Test,
   )
 
 }
