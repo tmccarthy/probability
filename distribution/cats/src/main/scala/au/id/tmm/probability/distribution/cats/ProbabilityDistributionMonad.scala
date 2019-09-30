887da2afd3b3174b1491266b8f8801f18bde5629
Timothy McCarthy <ebh042@gmail.com>
2019-09-30 14:42:38 +1000

    Scalafmt

--- distribution/cats/src/main/scala/au/id/tmm/probability/distribution/cats/ProbabilityDistributionMonad.scala
+++ distribution/cats/src/main/scala/au/id/tmm/probability/distribution/cats/ProbabilityDistributionMonad.scala
@@ -6,14 +6,17 @@ import cats.CommutativeMonad
 object ProbabilityDistributionMonad extends CommutativeMonad[ProbabilityDistribution] {
   override def pure[A](a: A): ProbabilityDistribution[A] = ProbabilityDistribution.always(a)
 
-  override def flatMap[A, B](fa: ProbabilityDistribution[A])(f: A => ProbabilityDistribution[B]): ProbabilityDistribution[B] =
+  override def flatMap[A, B](
+    fa: ProbabilityDistribution[A],
+  )(
+    f: A => ProbabilityDistribution[B],
+  ): ProbabilityDistribution[B] =
     fa.flatMap(f)
 
   @scala.annotation.tailrec
-  override def tailRecM[A, B](a: A)(f: A => ProbabilityDistribution[Either[A, B]]): ProbabilityDistribution[B] = {
+  override def tailRecM[A, B](a: A)(f: A => ProbabilityDistribution[Either[A, B]]): ProbabilityDistribution[B] =
     f(a).run() match {
       case Left(a1) => tailRecM(a1)(f)
       case Right(b) => pure(b)
     }
-  }
 }
