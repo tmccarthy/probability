6e286f2fc20e452779c95d80c587ed6e1abb3c9d
Timothy McCarthy <ebh042@gmail.com>
2019-10-13 13:58:33 +1100

    Add shrink and choose for DoubleProbability

--- shared/scalacheck/src/main/scala/au/id/tmm/probability/scalacheck/package.scala
+++ shared/scalacheck/src/main/scala/au/id/tmm/probability/scalacheck/package.scala
@@ -1,6 +1,8 @@
 package au.id.tmm.probability
 
-import org.scalacheck.{Arbitrary, Cogen, Gen}
+import com.github.ghik.silencer.silent
+import org.scalacheck.Gen.Choose
+import org.scalacheck.{Arbitrary, Cogen, Gen, Shrink}
 
 package object scalacheck {
 
@@ -10,8 +12,20 @@ package object scalacheck {
   implicit val doubleProbabilityCogen: Cogen[DoubleProbability] =
     Cogen.cogenDouble.contramap(_.asDouble)
 
-  // TODO double probability choose
+  implicit val doubleProbabilityChoose: Choose[DoubleProbability] =
+    Choose.xmap(DoubleProbability.makeUnsafe, _.asDouble)
 
-  // TODO double probability shrink
+  implicit val doubleProbabilityShrink: Shrink[DoubleProbability] = Shrink(doShrink)
+
+  @silent("deprecated")
+  private def doShrink(p: DoubleProbability): Stream[DoubleProbability] = p match {
+    case DoubleProbability.zero => Stream.empty
+    case p if Ordering[DoubleProbability].lt(p, DoubleProbability.makeUnsafe(1, 100000)) => Stream(DoubleProbability.zero)
+    case _ => {
+      val next = p / 2
+
+      next #:: doShrink(next)
+    }
+  }
 
 }
